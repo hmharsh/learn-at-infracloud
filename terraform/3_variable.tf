db6086c685d5bd6c1cb3ff4d5a7f50ace50df1e0
Harshit mahjan <hmharsh3@gmail.com>
2020-08-14 18:32:06 +0530

    Create 3_variable.tf

---
+++ terraform/3_variable.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  type = string
+  default = "harshit mahajan"  // will not ask for value of default is there at time of terraform apply 
+}
+
+resource "null_resource" "one" {
+provisioner "local-exec" {
+  command = "echo 'Hello ${var.name}'"
+}
+}
