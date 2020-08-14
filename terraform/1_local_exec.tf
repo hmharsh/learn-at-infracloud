180bdfd59697f1f04c016bffa895fbef7b11c2f5
Harshit mahjan <hmharsh3@gmail.com>
2020-08-14 17:19:19 +0530

    Update 1_local_exec.tf

--- terraform/1_local_exec.tf
+++ terraform/1_local_exec.tf
@@ -1,3 +1,6 @@
+
+// Provisioners can be used to model specific actions on the local machine or on a remote machine in order to prepare servers or other infrastructure objects for service.
+
 # If depends_on not used than all resource will run in parallel
 resource "null_resource" "first" {
     provisioner "local-exec" {
@@ -18,3 +21,19 @@ resource "null_resource" "third" {
         command = "echo 'third'"
     }
 }
+
+
+# one resource can also contain more than one provisioner also
+
+/*
+  
+resource "aws_instance" "web" {
+  # ...
+
+  provisioner "local-exec" {
+    command    = "echo The server's IP address is ${self.private_ip}"
+    on_failure = continue  // or 'fail'
+  }
+}
+
+*/
